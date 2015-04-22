072a2255383816c3489272184c39141a8192cd5d
AnanyaKumar <skywalker94@gmail.com>
2015-04-21 21:49:09 -0400

    Add localize interface with comments and types

---
+++ algorithms/localize_interface.py
@@ -0,0 +1,49 @@
+# Interface for localization algorithms
+
+class Localize_Interface:
+
+  def __init__(start_positions, motion_uncertainties, angle_uncertainties, 
+    distance_uncertainties):
+    """Initializes rover states with positions and uncertainty data.
+
+    Args:
+      start_positions: [ [x_0, y_0], [x_1, y_1] ] representing positions of the rovers, all floats
+      motion_uncertainties: [o_0, o_1] uncertainties in odometry data for rovers. Represents 1
+        standard deviation of odometry measurement if rovers move 1 meter. All floats.
+      angle_uncertainties: [a_0, a_1] uncertainties in angle data for rovers. Represents 1
+        standard deviation of angle measurement, in radians. All floats.
+      distance_uncertainties: [m_1, m_2] uncertainties in distance measurement. Represents 1
+        standard deviation of distance measurement if rovers are 1 meter apart. All floats.
+    """
+    pass
+
+  def measure_movement(distances_moved, directions):
+    """Update estimates of the rovers' poses using rover odometry/angle data.
+
+    Args:
+      distances_moved: list containing distance moved (measured) by the rovers in meters. All 
+        floats.
+      directions: list containing heading (measured) of each rover. All floats.
+    """
+    pass
+
+  def measure_distance(distances):
+    """Update estimates of rovers' poses using measured distance between the rovers.
+    
+    Args:
+      distances: [d_0, d_1] 
+        d_0 is distance measured by rover 0 to rover 1, in meters (float).
+        d_1 is distance measured by rover 1 to rover 0, in meters (float).
+    """
+    pass
+
+  def get_pose_estimate(rover_idx):
+    """Returns the best estimate of the specified rover's pose.
+
+    Args:
+      rover_idx: rover index (int).
+
+    Returns:
+      [x, y] representing the best estiamte of the rover's pose. All floats.
+    """
+    pass
